23a5ef103c566b3a8b5722270a5dc63ea2212564
Kalyan Momidi <ms@Kalyans-MacBook-Air.local>
2026-04-15 12:33:03 -0600

    hello world chain

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+from json import load
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def main():
+    print("Hello from langchain-course!")
+    print(os.environ.get("GOOGLE_API_KEY"))
+
+
+if __name__ == "__main__":
+    main()
